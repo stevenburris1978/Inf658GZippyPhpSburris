e0bceba1242b591e511e3e38c4d98cc1563dbbf8
skylor burris <s_burris@mail.fhsu.edu>
2024-03-18 14:20:34 -0500

    Zippy add database

--- model/database.php
+++ model/database.php
@@ -1,10 +1,10 @@
 <?php
-$dsn ="mysql:host=localhost; dbname=zippyusedautos";
-$username = 'root';
-// $password = '1qaz';
+$dsn ="mysql:host=wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vhnv769j2d04wurh";
+$username = 'vdmrkq9nc5te7aws';
+$password = 'icm3lxx2cxk96pse';
 
 try{
-$db = new PDO($dsn, $username);
+$db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
 $error_message = 'Database Error!';
 $error_message .= $e->getMessage();
